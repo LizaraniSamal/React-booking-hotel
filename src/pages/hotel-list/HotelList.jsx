8c303334afc80f7596ace48f99ffffc359aa1a52
lizaranisamal <lizaranisamal1@gmail.com>
2024-04-07 19:59:51 +0530

    add new feature

--- src/pages/hotel-list/HotelList.jsx
+++ src/pages/hotel-list/HotelList.jsx
@@ -7,12 +7,14 @@ import format from "date-fns/format";
 import { DateRange } from "react-date-range";
 import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
+import SearhItem from "../../components/searchItem/SearhItem";
 const HotelList = () => {
   const location = useLocation();
   const [destination, setDestination] = useState(location.state.destination);
   const [dateRange, setDateRange] = useState(location.state.dateRange);
   const [options, setOptions] = useState(location.state.options);
-  // console.log(dateRange);
+  const [openDate, setOpenDate] = useState(false);
+  //console.log(dateRange);
 
   return (
     <>
@@ -30,15 +32,79 @@ const HotelList = () => {
 
               <div className="isItem">
                 <label>Check-in date</label>
-                <span>{`${format(dateRange[0].startDate, "MM/dd/yyyy")} to ${
-                  (dateRange[0].endDate, "MM/dd/yyyy")
-                } `}</span>
-                {/* <DateRange
-                      onChange = {}
-                    /> */}
+                <span onClick={() => setOpenDate(!openDate)}>{`${format(
+                  dateRange[0].startDate,
+                  "MM/dd/yyyy"
+                )} to ${format(dateRange[0].endDate, "MM/dd/yyyy")} `}</span>
+                {openDate && (
+                  <DateRange
+                    onChange={(item) => setDateRange([item.selection])}
+                    minDate={new Date()}
+                    ranges={dateRange}
+                  />
+                )}
               </div>
+              <div className="isItem">
+                <label>Options</label>
+                <div className="lsItemOptions">
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">
+                      Min price <small>per night</small>
+                    </span>
+                    <input type="number" className="lsOptionInput"></input>
+                  </div>
+
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">
+                      Max price <small>per night</small>
+                    </span>
+                    <input type="number" className="lsOptionInput"></input>
+                  </div>
+
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">Adult</span>
+                    <input
+                      type="number"
+                      min={1}
+                      className="lsOptionInput"
+                      placeholder={options.adult}
+                    ></input>
+                  </div>
+
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">Children</span>
+                    <input
+                      type="number"
+                      min={0}
+                      className="lsOptionInput"
+                      placeholder={options.children}
+                    ></input>
+                  </div>
+
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">Room</span>
+                    <input
+                      type="number"
+                      min={1}
+                      className="lsOptionInput"
+                      placeholder={options.room}
+                    ></input>
+                  </div>
+                </div>
+              </div>
+              <button>Search</button>
             </div>
-            <div className="listResult"></div>
+            <div className="listResult">
+              <SearhItem />
+              <SearhItem />
+              <SearhItem />
+              <SearhItem />
+              <SearhItem />
+              <SearhItem />
+              <SearhItem />
+              <SearhItem />
+              <SearhItem />
+            </div>
           </div>
         </div>
       </div>
